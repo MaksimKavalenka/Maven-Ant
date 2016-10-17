5c7e202502133980583ded975503a8136e2d7906
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2016-10-18 01:08:39 +0300

    Task 1 (Apache Maven)

---
+++ Task 1 (Apache Maven)/maven-training-parent/maven-training/src/main/java/com/epam/training/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.epam.training;
+
+public class HelloWorld {
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+
+}
